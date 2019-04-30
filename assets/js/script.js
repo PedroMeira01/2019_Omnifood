3ddffc4b9fb071a0223a740cb865f2b284ea46dc
Pedro <pedrohenrique.peu09@gmail.com>
2019-04-30 10:41:27 -0300

    Scrolling Effects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 
 
+    // STICKY NAVIGATION
+
     $('.js--section-features').waypoint(function(direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -11,10 +13,18 @@ $(document).ready(function() {
             offset: '60px'
           });
 
-    // var waypoints = $('#handler-first').waypoint(function(direction) {
-    //     notify(this.element.id + ' hit 25% from top of window') 
-    //   }, {
-    //     offset: '25%'
-    //   })
+              // SCROLL ON BUTTONS
 
-});
+              $('.js--scroll-to-plans').click(function() {
+                  $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 2500);
+              });
+
+              $('.js-scroll-to-start').click(function() {
+                $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 2500);
+            });
+    });
+
+
+
+
+
